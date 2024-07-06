3930212255d8badb4af5694205091aed27d8f31e
Wemegah <swemegah001@st.ug.edu.gh>
2024-07-06 10:22:41 +0100

    modified packages

--- InheritanceMethodOverriding/obj/Debug/net8.0/InheritanceMethodOverriding.AssemblyInfo.cs
+++ InheritanceMethodOverriding/obj/Debug/net8.0/InheritanceMethodOverriding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InheritanceMethodOverriding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f27c9904dc0480531682c14dfc8d6dab1180ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b76bb28f8976d6227b899790bc89014e4283f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("InheritanceMethodOverriding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InheritanceMethodOverriding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
